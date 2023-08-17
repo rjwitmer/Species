c2685432adc963a43fcee82c2587d979943794e3
Bob Witmer <bwitmer@golden.net>
2023-08-17 11:33:54 -0400

    Initial Commit

---
+++ Species/SpeciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeciesApp.swift
+//  Species
+//
+//  Created by Bob Witmer on 2023-08-17.
+//
+
+import SwiftUI
+
+@main
+struct SpeciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
